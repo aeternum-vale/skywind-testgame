c03548b8fc6c49e8b19b34ee23310946b552fc05
aeternum-vale <aeternum-vale@ya.ru>
2019-02-16 22:47:35 +0300

    add settings files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.ts",
+
+  output: {
+    path: path.resolve(__dirname, "game"),
+    filename: "bundle.js"
+  },
+
+  devtool: "source-map",
+  resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/
+      },
+      { 
+        test: /\.tsx?$/, 
+        loader: "awesome-typescript-loader" },
+      { 
+        enforce: "pre", 
+        test: /\.js$/, loader: "source-map-loader" 
+      }
+    ]
+  }
+};
