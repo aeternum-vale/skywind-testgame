2d922f9729b3b3b53a11aed093f3d94e91cc3fec
aeternum-vale <aeternum-vale@ya.ru>
2019-02-17 23:09:14 +0300

    implement mouse event handlers

--- src/classes/Button.ts
+++ src/classes/Button.ts
@@ -1,10 +1,8 @@
 import * as PIXI from "pixi.js";
-import { GameObject } from "./GameObject";
+import { IGameObject } from "./GameObject";
 
 enum ButtonState {
     Normal,
-    Hover,
-    Pressed,
     Disable
 }
 
@@ -20,29 +18,71 @@ interface IButtonOptions {
     width?: number;
     height?: number;
     textures: IButtonTextures;
+    onClickCallback?: () => void;
 }
 
-export class Button extends GameObject {
+export class Button implements IGameObject {
+    private _onClickCallback: () => void;
+    private _buttonSprite: PIXI.Sprite;
     private _state: ButtonState = ButtonState.Normal;
+    private _textures: IButtonTextures;
 
     constructor(options: IButtonOptions) {
-        super();
         const {
             textures,
             position = new PIXI.Point(0, 0),
             width,
-            height
+            height,
+            onClickCallback
         } = options;
 
-        this._displayObject = new PIXI.Sprite(textures.normal);
-        this._displayObject.position = position;
+        this._onClickCallback = onClickCallback;
+        this._textures = textures;
+        this._buttonSprite = new PIXI.Sprite(textures.normal);
+        this._buttonSprite.position = position;
         if (width) {
-            this._displayObject.width = width;
+            this._buttonSprite.width = width;
         }
         if (height) {
-            this._displayObject.height = height;
+            this._buttonSprite.height = height;
         }
+
+        this._buttonSprite.interactive = true;
+        this._buttonSprite.mouseover  = this._onMouseOver.bind(this);
+        this._buttonSprite.mouseout  = this._onMouseOut.bind(this);
+        this._buttonSprite.click  = this._onClick.bind(this);
+    }
+
+    public getDisplayObject(): PIXI.Sprite {
+        return this._buttonSprite;
+    }
+
+    public activate() {
+        this._state = ButtonState.Normal;
+        this._buttonSprite.texture = this._textures.normal;
     }
 
     public update(delta: number) {}
+
+    private _onMouseOver(e: PIXI.interaction.InteractionEvent): void {
+        if (this._state === ButtonState.Normal) {
+            this._buttonSprite.texture = this._textures.hover;
+        }
+    }
+
+    private _onMouseOut(e: PIXI.interaction.InteractionEvent): void {
+        if (this._state === ButtonState.Normal) {
+            this._buttonSprite.texture = this._textures.normal;
+        }
+    }
+
+    private _onClick(e: PIXI.interaction.InteractionEvent): void {
+        if (this._state === ButtonState.Normal) {
+            this._state = ButtonState.Disable;
+            this._buttonSprite.texture = this._textures.disable;
+            if (this._onClickCallback) {
+                this._onClickCallback.call(this);
+            }
+        }
+    }
 }
